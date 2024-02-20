a8d481c93ea94b72df02d4ee99e83ac592f6c004
marshmallow <marshmallow@marks-MBP.mshome.net>
2024-02-20 16:38:47 +0800

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+// ng material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent, 
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatInputModule, MatButtonModule,
+    MatCardModule, MatToolbarModule,
+    MatMenuModule, MatIconModule,
+    MatExpansionModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
